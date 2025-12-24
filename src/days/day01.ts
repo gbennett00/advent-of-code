b8413448545e870a74378cf93778fdae5379299b
Garrett B <garrett.bennett@neighbor.com>
2025-12-24 10:12:33 -0700

    Day 1 - Part 2

--- src/days/day01.ts
+++ src/days/day01.ts
@@ -9,9 +9,16 @@ let dial = 50
 let zeros = 0
 for (const line of lines) {
     let diff = line[0] === 'R' ? Number(line.slice(1)) : -Number(line.slice(1))
-    dial += diff
-    dial = (dial + 100) % 100
-    if (dial === 0) zeros++
+    while (diff < 0) {
+        dial = (dial - 1 + 100) % 100
+        if (dial === 0) zeros++
+        diff++
+    }
+    while (diff > 0) {
+        dial = (dial + 1) % 100
+        if (dial === 0) zeros++
+        diff--
+    }
 }
 
 console.log("total zeros: ", zeros)
