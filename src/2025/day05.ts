cc236e4374010008ee4db14b3f41a9624883020a
Garrett B <garrett.bennett@neighbor.com>
2025-12-25 14:23:34 -0700

    Day 5 - Part 2

--- src/2025/day05.ts
+++ src/2025/day05.ts
@@ -6,17 +6,31 @@ const day = 5;
 const lines = readLines(year, day);
 
 const blank = lines.findIndex(l => l.length === 0)
-const ranges = lines.slice(0, blank).map(toRange)
-const ids = lines.slice(blank+1).map(Number)
+const ranges = lines.slice(0, blank).map(toRange).sort((a, b) =>  (a[0] ?? 0) - (b[0] ?? 0))
 
-console.log(ids.filter(id => isFresh(id)).length)
-
-function isFresh(id: number) {
-    for (const range of ranges) {
-        const start = range[0]
-        const end = range[1]
-        if (!start || !end) continue
-        if (id >= start && id <= end) return true
+// combine the ranges
+let i = 0
+while (i < ranges.length - 1) {
+    const a = ranges[i]
+    const b = ranges[i+1]
+    if (!a || !b) break
+    const a1 = a[1] ?? 0
+    const b0 = b[0] ?? 0
+    if (b0 <= a1) {
+        a[1] = Math.max(a1, (b[1] ?? 0))
+        ranges.splice(i+1, 1)
+    } else {
+        i++
     }
-    return false
 }
+
+// count the size of all ranges
+let sum = 0
+for (const range of ranges) {
+    const a = range[0]
+    const b = range[1]
+    if (!a || !b) continue
+    sum += (b - a + 1)
+}
+
+console.log(sum)
