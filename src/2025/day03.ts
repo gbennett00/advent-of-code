9425e0427338560509628c77796eb0d2b2699d40
Garrett B <garrett.bennett@neighbor.com>
2025-12-24 22:01:44 -0700

    Day 3 - Part 2

--- src/2025/day03.ts
+++ src/2025/day03.ts
@@ -12,20 +12,16 @@ for (const line of lines) {
 console.log(totalJoltage)
 
 function calculateJoltage(line: string) {
-    let firstMax = line[0] ?? '0'
-    let firstMaxIdx = 0
-    for (let i = 0; i < line.length-1; i++) {
-        const curr = line[i] ?? '0'
-        if (curr > firstMax) { 
-            firstMax = curr
-            firstMaxIdx = i
+    const maxes = new Array<string>(12).fill('0')
+    let nextIndex = 0
+    for (let i = 0; i < 12; i++) {
+        for (let j = nextIndex; j < line.length-11+i; j++) {
+            const curr = line[j] ?? '0'
+            if (curr > (maxes[i] ?? 0)) {
+                maxes[i] = curr
+                nextIndex = j + 1
+            }
         }
     }
-
-    let secondMax = line[firstMaxIdx + 1] ?? '0'
-    for (let i = firstMaxIdx + 1; i < line.length; i++) {
-        const curr = line[i] ?? '0'
-        if (curr > secondMax) secondMax = curr
-    }
-    return (Number(firstMax) * 10) + Number(secondMax)
+    return Number(maxes.join(''))
 }
